1b683f0c542f5fc1a4d2f75b7274be89178376b7
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2024-10-15 09:55:50 +0200

    Ricardo -> Master

--- bar_plotting.py
+++ bar_plotting.py
@@ -1,34 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_bar_chart(labels, train_scores, test_means, test_stds, title='Scores by Model and Dataset', ylabel='Scores'):
+
+def plot_bar_chart(labels, train_scores, test_score_medians, test_score_mins, test_score_maxs,
+                   ylabel='AUC'):
     """
-    Plots a bar chart comparing train scores and test scores with error bars.
+    Saves a bar chart comparing train scores and test scores with error bars.
 
     Parameters:
     - labels (list of str): Labels for each group (e.g., model types).
-    - train_scores (list of float): Training scores for each group.
-    - test_means (list of float): Mean test scores for each group.
-    - test_stds (list of float): Standard deviations of test scores for each group.
-    - title (str): Title of the chart.
+    - train_scores (array of float): Training scores for each group.
+    - test_score_medians (array of float): Median test scores for each group.
+    - test_score_mins (array of float): Minimum test scores for each group.
+    - test_score_maxs (array of float): Maximum test scores for each group.
     - ylabel (str): Label for the y-axis.
 
     Returns:
-    - None: Displays the bar chart.
+    - None: Saves the bar chart.
     """
     x = np.arange(len(labels))
     width = 0.2
 
     fig, ax = plt.subplots()
 
-    rects1 = ax.bar(x - width / 2, train_scores, width, label='Train Scores')
-    rects2 = ax.bar(x + width / 2, test_means, width, yerr=test_stds, label='Test Scores', capsize=5)
-
+    rects1 = ax.bar(x - width / 2, train_scores, width, label='Train AUC')
+    rects2 = ax.bar(x + width / 2, test_score_medians, width, yerr=np.stack(
+        [test_score_medians - test_score_mins, test_score_maxs - test_score_medians]),
+        label='Test AUC', capsize=5)
+    
     ax.set_ylabel(ylabel)
-    ax.set_title(title)
     ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.legend()
+    ax.set_xticklabels(labels, rotation=45)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=2)
 
     def autolabel(rects):
         for rect in rects:
@@ -43,4 +46,4 @@ def plot_bar_chart(labels, train_scores, test_means, test_stds, title='Scores by
     autolabel(rects2)
 
     fig.tight_layout()
-    plt.show()
+    plt.savefig("bar_chart.pdf")
