aa79c9eb32308d253def19c3b01397a60b875902
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-01-18 11:54:39 +0100

    test gpu

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from run_models import run_all_models
+# from run_models import run_all_models
 # from run_new import run
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    run_all_models()
+    import torch
+
+    print(torch.cuda.is_available())  # Should return True
+    print(torch.cuda.device_count())  # Should show the number of GPUs
+    print(torch.version.cuda)  # Should return the CUDA version PyTorch is using
+
+    # run_all_models()
     # run()
     # create_summaries()
