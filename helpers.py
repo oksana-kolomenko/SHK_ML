7653df1721726d370bb6cd81bd19017bbfd4451d
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-01-19 23:42:01 +0100

    kf

--- helpers.py
+++ helpers.py
@@ -662,6 +662,8 @@ def combine_data(X_tabular, summaries, feature_extractor):
                     print(f"Shape of first element: {len(t[0])} (if it's a list)")
             # print(f"Content preview: {t[:2]}")  # Show the first 2 elements of `t` (if it's a list)
             print("---")
+            if len(t.shape) == 1:
+                t = t.reshape(-1, 1)
         print(f"Ammount of embs: {len(text_embeddings)}")
         # Ensure the embeddings are 2D
         # if len(text_embeddings.shape) == 1:
