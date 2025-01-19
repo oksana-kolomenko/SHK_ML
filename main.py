6019e77574af7ae3c6b28bad6c5c49bf1ba50b6c
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-01-19 22:50:49 +0100

    kf

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import time
 import numpy as np
 from helpers import load_summaries
-# from run_models_text_emb import run_models_on_txt_emb
+from run_models_text_emb import run_models_on_txt_emb
 # from run_models_concatenated import run_models_concatenated
 
 
@@ -18,9 +18,9 @@ def dummy_function():
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
-    patient_summaries = load_summaries()
-    print(len(patient_summaries))
-    # run_models_on_txt_emb()
+    #patient_summaries = load_summaries()
+    #print(len(patient_summaries))
+     run_models_on_txt_emb()
     # run_models_on_table_data()
     # run()
     # create_summaries()
