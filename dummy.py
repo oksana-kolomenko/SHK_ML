78745a710fd1ea6eff7b3eb6acf87827aa7a86ab
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-04-07 15:06:36 +0200

    blub

--- dummy.py
+++ dummy.py
@@ -1,12 +1,14 @@
-from transformers import AutoTokenizer, AutoModel
-from transformers import pipeline
-
+import torch
+from transformers import AutoTokenizer, AutoModel, pipeline
 
 def test():
-    model_name = "dunzhang/stella_en_400M_v5"  # or correct name
+    model_name = "dunzhang/stella_en_400M_v5"
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
 
-    pipe = pipeline("feature-extraction", model=model, tokenizer=tokenizer)
+    # move model to GPU
+    model = model.to("cuda")
+
+    pipe = pipeline("feature-extraction", model=model, tokenizer=tokenizer, device=0)
     output = pipe("This is a test sentence.")
     print(len(output), len(output[0]))
