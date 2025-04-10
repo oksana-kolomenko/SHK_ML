774d1fc49779da08442077b1da8d8c8036f1f97d
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-04-10 22:27:38 +0200

    small fix

--- run_models_pca.py
+++ run_models_pca.py
@@ -175,7 +175,7 @@ def run_pca_txt_emb():
 
         # Logistic Regression
         # concatenation 1
-        """(lr_conc_dataset, lr_conc_ml_method, lr_conc_emb_method,
+        (lr_conc_dataset, lr_conc_ml_method, lr_conc_emb_method,
          lr_conc_yesno, lr_best_params, lr_pca_components, lr_conc_train_score,
          lr_conc_test_scores) = concat_lr_txt_emb(
             dataset_name=posttrauma_dataset,
@@ -207,11 +207,10 @@ def run_pca_txt_emb():
                             best_params=lr_best_params,
                             pca_n_comp=lr_pca_components,
                             metrics=lr_conc_test_scores,
-                            is_train=False)"""
-
+                            is_train=False)
 
         # HGBC conc pca
-        (concat_hgbc_dataset, concat_hgbc_ml_method, concat_hgbc_emb_method,
+        """(concat_hgbc_dataset, concat_hgbc_ml_method, concat_hgbc_emb_method,
          hgbc_conc_yesno, hgbc_best_params, hgbc_pca_components, hgbc_conc_train_score,
          hgbc_conc_test_scores) = concat_txt_hgbc(
             dataset_name=posttrauma_dataset,
@@ -241,7 +240,7 @@ def run_pca_txt_emb():
                             best_params=hgbc_best_params,
                             pca_n_comp=hgbc_pca_components,
                             metrics=hgbc_conc_test_scores,
-                            is_train=False)
+                            is_train=False)"""
 
         # Logistic Regression
         # concatenation 2
