62fd4fb109aa2933476834e6baa575e754b692e5
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-04-10 19:10:01 +0200

    small fix

--- helpers.py
+++ helpers.py
@@ -631,7 +631,7 @@ def concat_lr_txt_emb(dataset_name, emb_method,
         if n_components else "none"
 
     is_sentence_transformer = False
-    if "gtr-t5-base" in emb_method.lower() or "sentence-t5-base" in emb_method.lower() or "modernbert_embed" in emb_method.lower():
+    if "gtr_t5_base" in emb_method.lower() or "sentence_t5_base" in emb_method.lower() or "modernbert_embed" in emb_method.lower():
         is_sentence_transformer = True
 
     pipeline_text_steps = [
