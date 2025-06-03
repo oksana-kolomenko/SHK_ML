9765381f0399bb4756d840db7cfeb64bb4357dd2
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-06-03 13:52:20 +0200

    upd

--- run_models_pca.py
+++ run_models_pca.py
@@ -72,7 +72,7 @@ def run_pca_txt_emb():
         # "all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
 
         # GTR T5 Base
-        "GTR_T5_Base": feature_extractor_gtr_t5_base,
+        #"GTR_T5_Base": feature_extractor_gtr_t5_base,
 
         # Sentence T5 Base
         "sentence_t5_base": feature_extractor_sentence_t5_base,
