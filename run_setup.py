5de51770959478cef8260faefe8d912dfb7def70
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-07-26 11:13:29 +0200

    upd

--- run_setup.py
+++ run_setup.py
@@ -81,37 +81,36 @@ def run_pca_txt_emb(feature_extractor_gtr_t5_base=None):
 
     methods = {
         # all summaries, all features
-
-        "pca_conc1": {"X": X,
-                      "summaries": all_summaries,
-                      "conc": "conc1",
-                      "pca": True,
-                      "pca_str": "pca_"},
+        #"pca_conc1": {"X": X,
+         #             "summaries": all_summaries,
+         #             "conc": "conc1",
+         #             "pca": True,
+         #             "pca_str": "pca_"},
 
         # all summaries, metr features
-        "pca_conc2": {"X": X_metr,
-                      "summaries": all_summaries,
-                      "conc": "conc2",
-                      "pca": True,
-                      "pca_str": "pca_"},
+        #"pca_conc2": {"X": X_metr,
+        #              "summaries": all_summaries,
+        #              "conc": "conc2",
+        #              "pca": True,
+        #              "pca_str": "pca_"},
         # nom summaries, metr features
-        "pca_conc3": {"X": X_metr,
-                      "summaries": nom_summaries,
-                      "conc": "conc3",
-                      "pca": True,
-                      "pca_str": "pca_"},
+        #"pca_conc3": {"X": X_metr,
+        #              "summaries": nom_summaries,
+        #              "conc": "conc3",
+        #              "pca": True,
+        #              "pca_str": "pca_"},
         # all summaries, all features
-        "conc1": {"X": X,
-                  "summaries": all_summaries,
-                  "conc": "conc1",
-                  "pca": False,
-                  "pca_str": ""},
+        #"conc1": {"X": X,
+        #          "summaries": all_summaries,
+        #          "conc": "conc1",
+        #          "pca": False,
+        #          "pca_str": ""},
         # all summaries, metr features
-        "conc2": {"X": X_metr,
-                  "summaries": all_summaries,
-                  "conc": "conc2",
-                  "pca": False,
-                  "pca_str": ""},
+        #"conc2": {"X": X_metr,
+        #          "summaries": all_summaries,
+        #          "conc": "conc2",
+        #          "pca": False,
+        #          "pca_str": ""},
         # nom summaries, metr features
         "conc3": {"X": X_metr,
                   "summaries": nom_summaries,
@@ -124,10 +123,10 @@ def run_pca_txt_emb(feature_extractor_gtr_t5_base=None):
 
     feature_extractors = {
         # All MiniLM L6 v2
-        "all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
+        #"all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
 
         # Stella en 400m v5
-        "Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
+        #"Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
 
         # GTR T5 Base
         #"GTR_T5_Base": feature_extractor_gtr_t5_base,
@@ -136,28 +135,28 @@ def run_pca_txt_emb(feature_extractor_gtr_t5_base=None):
         #"sentence_t5_base": feature_extractor_sentence_t5_base,
 
         # Ember v1
-        "ember_v1": feature_extractor_ember_v1,
+        #"ember_v1": feature_extractor_ember_v1,
 
         # E5 Models
-        "E5-Small-V2": feature_extractor_e5_small_v2,
-        "E5-Base-V2": feature_extractor_e5_base_v2,
+        #"E5-Small-V2": feature_extractor_e5_small_v2,
+        #"E5-Base-V2": feature_extractor_e5_base_v2,
         "E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
-        "BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
-        "BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
-        "BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
+        #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
+        #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
+        #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
 
         # GIST Models
-        "GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
-        "GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
-        "GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
+        #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
+        #"GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
+        #"GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
 
         # GTE Models
-        "GTE-Small": feature_extractor_gte_small,
-        "GTE-Base": feature_extractor_gte_base,
-        "GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
-        "GTE-Large": feature_extractor_gte_large,
+        #"GTE-Small": feature_extractor_gte_small,
+        #"GTE-Base": feature_extractor_gte_base,
+        #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
+        #"GTE-Large": feature_extractor_gte_large,
 
         # Potion Models
         # "Potion-Base-2M": feature_extractor_potion_base_2M,
@@ -309,7 +308,7 @@ def run_pca_txt_emb(feature_extractor_gtr_t5_base=None):
 
             # Logistic Regression conc (pca)
 
-            """(lr_conc_dataset, lr_conc_ml_method, lr_conc_emb_method,
+            (lr_conc_dataset, lr_conc_ml_method, lr_conc_emb_method,
              lr_conc_yesno, lr_best_params, lr_pca_components, lr_conc_train_score,
              lr_conc_test_scores) = concat_lr_txt_emb(
                 dataset_name=dataset,
@@ -342,10 +341,10 @@ def run_pca_txt_emb(feature_extractor_gtr_t5_base=None):
                                 best_params=lr_best_params,
                                 pca_n_comp=lr_pca_components,
                                 metrics=lr_conc_test_scores,
-                                is_train=False)"""
+                                is_train=False)
 
             # HGBC conc (pca)
-            (concat_hgbc_dataset, concat_hgbc_ml_method, concat_hgbc_emb_method,
+            """(concat_hgbc_dataset, concat_hgbc_ml_method, concat_hgbc_emb_method,
              hgbc_conc_yesno, hgbc_best_params, hgbc_pca_components, hgbc_conc_train_score,
              hgbc_conc_test_scores) = concat_hgbc_txt_emb(
                 dataset_name=dataset,
@@ -374,7 +373,7 @@ def run_pca_txt_emb(feature_extractor_gtr_t5_base=None):
                                 best_params=hgbc_best_params,
                                 pca_n_comp=hgbc_pca_components,
                                 metrics=hgbc_conc_test_scores,
-                                is_train=False)
+                                is_train=False)"""
 
 
 def run_pca_rte():
