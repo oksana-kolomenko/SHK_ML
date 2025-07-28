c17491f30c819229a2edf27272ab98502c5caa2e
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-07-28 07:42:37 +0200

    ld hgbc conc

--- run_setup.py
+++ run_setup.py
@@ -3,16 +3,16 @@ import numpy as np
 from csv_saver import save_results_to_csv
 from data_preps import load_features, load_labels, load_summaries
 from helpers import concat_lr_rte, concat_hgbc_rte, concat_lr_txt_emb, concat_hgbc_txt_emb, hgbc_txt_emb, lr_txt_emb
-from models import feature_extractor_e5_large_v2, feature_extractor_gist_embedding_v0, feature_extractor_gte_base, \
+"""from models import feature_extractor_e5_large_v2, feature_extractor_gist_embedding_v0, feature_extractor_gte_base, \
     feature_extractor_gte_base_en_v1_5, feature_extractor_gte_large, feature_extractor_bge_base_en_v1_5, \
     feature_extractor_bge_large_en_v1_5, feature_extractor_gist_large_embedding_v0, feature_extractor_sentence_t5_base, \
-    feature_extractor_gtr_t5_base
+    feature_extractor_gtr_t5_base"""
 
 #from helpers import concat_lr_rte, concat_hgbc_rte, lr_txt_emb, hgbc_txt_emb, concat_lr_txt_emb, concat_hgbc_txt_emb
-from models import (feature_extractor_bge_base_en_v1_5, feature_extractor_bge_large_en_v1_5,\
+from models import (feature_extractor_all_minilm_l6_v2, feature_extractor_bge_base_en_v1_5, feature_extractor_bge_large_en_v1_5,\
     feature_extractor_gist_large_embedding_v0, feature_extractor_bge_small_en_v1_5, \
     feature_extractor_gist_small_embedding_v0, feature_extractor_gte_small, feature_extractor_e5_small_v2, \
-    feature_extractor_e5_base_v2, feature_extractor_stella_en_400M_v5, feature_extractor_all_minilm_l6_v2,
+    feature_extractor_e5_base_v2, feature_extractor_stella_en_400M_v5,
                     feature_extractor_ember_v1)
 from values import DatasetName
 
@@ -123,7 +123,7 @@ def run_txt_emb():
 
     feature_extractors = {
         # All MiniLM L6 v2
-        #"all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
+        "all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
 
         # Stella en 400m v5
         #"Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
@@ -138,19 +138,19 @@ def run_txt_emb():
         #"ember_v1": feature_extractor_ember_v1,
 
         # E5 Models
-        #"E5-Small-V2": feature_extractor_e5_small_v2,
-        #"E5-Base-V2": feature_extractor_e5_base_v2,
-        #"E5-Large-V2": feature_extractor_e5_large_v2,
+        "E5-Small-V2": feature_extractor_e5_small_v2,
+        "E5-Base-V2": feature_extractor_e5_base_v2,
+        "E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
-        #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
-        #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
-        #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
+        "BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
+        "BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
+        "BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
 
         # GIST Models
-        #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
-        #"GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
-        #"GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
+        "GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
+        "GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
+        "GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
 
         # GTE Models
         #"GTE-Small": feature_extractor_gte_small,
