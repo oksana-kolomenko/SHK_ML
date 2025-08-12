3b341eb995891ffdfe1527fb677d007185deae58
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-08-12 09:54:59 +0200

    mimic1 lr rests

--- run_setup.py
+++ run_setup.py
@@ -63,8 +63,8 @@ def run_txt_emb():
     ]"""
 
     # === MIMIC ===
-    task = "task_0"
-    dataset = DatasetName.MIMIC_0.value
+    task = "task_1"
+    dataset = DatasetName.MIMIC_1.value
 
     y_train = load_labels(f"mimic_data/y_train_{task}.csv")
     y_test = load_labels(f"mimic_data/y_test_{task}.csv")
@@ -139,12 +139,12 @@ def run_txt_emb():
         # GIST Models
         #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
         #"GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
-        #"GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
+        "GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
 
         # GTE Models
-        #"GTE-Small": feature_extractor_gte_small,
-        #"GTE-Base": feature_extractor_gte_base,
-        #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
+        "GTE-Small": feature_extractor_gte_small,
+        "GTE-Base": feature_extractor_gte_base,
+        "GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
         "GTE-Large": feature_extractor_gte_large,
 
         # GTR T5 Base
