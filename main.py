1eb0b3cdebf9c0d7ab2e8ee8a36e28910722ef19
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-08-17 14:28:43 +0200

    Korrektur

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from dummy import print_special_tokens, print_sentence_embedding
 #from data_prep import mimic_convert_binary_to_bool, mimic_generate_tasks, print_csv_file_lengths, \
 #    mimic_add_preprocessing, do_subsample, do_add_prep, create_mimic_summaries
 
-#from run_setup import run_txt_emb
+from run_setup import run_txt_emb
 #from dummy import test
 #from run_models_table_data import run_models_on_table_data
 
 if __name__ == '__main__':
-    #run_txt_emb()
+    run_txt_emb()
     # MIMIC
     # 1. Create tasks
     # mimic_generate_tasks
@@ -25,5 +25,5 @@ if __name__ == '__main__':
     # 3. Replace 1/0 and True/False with yes/no
     #do_add_prep()
     # 4. Create Summaries
-    create_mimic_summaries()
+    #create_mimic_summaries()
     # print_csv_file_lengths("..\mimic\subsampled")
