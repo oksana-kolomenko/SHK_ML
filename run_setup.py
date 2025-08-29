96a478f31c9b57d344413ca8e8a1a244a3caa07b
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-08-29 23:20:25 +0200

    mm0 hgbc base

--- run_setup.py
+++ run_setup.py
@@ -64,8 +64,8 @@ def run_txt_emb():
     ]"""
 
     # === MIMIC ===
-    task = "task_2"
-    dataset = DatasetName.MIMIC_2.value
+    task = "task_0"
+    dataset = DatasetName.MIMIC_0.value
 
     X_train = load_features(f"mimic_data/X_train_{task}.csv")
     X_test = load_features(f"mimic_data/X_test_{task}.csv")
@@ -144,7 +144,7 @@ def run_txt_emb():
 
     feature_extractors = {
         # All MiniLM L6 v2
-        "all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
+        #"all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
 
         # Stella en 400m v5
         #"Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
@@ -153,22 +153,22 @@ def run_txt_emb():
         #"ember_v1": feature_extractor_ember_v1,
 
         # E5 Models
-        "E5-Small-V2": feature_extractor_e5_small_v2,
-        #"E5-Base-V2": feature_extractor_e5_base_v2,
+        #"E5-Small-V2": feature_extractor_e5_small_v2,
+        "E5-Base-V2": feature_extractor_e5_base_v2,
         #"E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
-        "BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
-        #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
+        #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
+        "BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
         #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
 
         # GIST Models
-        "GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
+        #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
         #"GIST-Embedding-v0": feature_extractor_gist_embedding_v0,
         #"GIST-Large-Embedding-v0": feature_extractor_gist_large_embedding_v0,
 
         # GTE Models
-        "GTE-Small": feature_extractor_gte_small,
+        #"GTE-Small": feature_extractor_gte_small,
         #"GTE-Base": feature_extractor_gte_base,
         #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
         #"GTE-Large": feature_extractor_gte_large,
