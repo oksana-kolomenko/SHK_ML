c2cdc229fe601a42c563e8f12a2e3e43e6475243
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-08-31 11:29:09 +0200

    m1hgbc allmini

--- run_setup.py
+++ run_setup.py
@@ -144,7 +144,7 @@ def run_txt_emb():
 
     feature_extractors = {
         # All MiniLM L6 v2
-        #"all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
+        "all_miniLM_L6_v2": feature_extractor_all_minilm_l6_v2,
 
         # Stella en 400m v5
         #"Stella-EN-400M-v5": feature_extractor_stella_en_400M_v5,
@@ -155,12 +155,12 @@ def run_txt_emb():
         # E5 Models
         #"E5-Small-V2": feature_extractor_e5_small_v2,
         #"E5-Base-V2": feature_extractor_e5_base_v2,
-        "E5-Large-V2": feature_extractor_e5_large_v2,
+        #"E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
         #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
         #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
-        "BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
+        #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
 
         # GIST Models
         #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
