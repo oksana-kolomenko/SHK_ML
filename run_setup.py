070e67be8ce41aac01022281c7c92d34b8a4a8fc
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-08-31 11:27:15 +0200

    m1hgbc E5&BGE Large

--- run_setup.py
+++ run_setup.py
@@ -155,12 +155,12 @@ def run_txt_emb():
         # E5 Models
         #"E5-Small-V2": feature_extractor_e5_small_v2,
         #"E5-Base-V2": feature_extractor_e5_base_v2,
-        #"E5-Large-V2": feature_extractor_e5_large_v2,
+        "E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
         #"BGE-Small-EN-v1.5": feature_extractor_bge_small_en_v1_5,
         #"BGE-Base-EN-v1.5": feature_extractor_bge_base_en_v1_5,
-        #"BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
+        "BGE-Large-EN-v1.5": feature_extractor_bge_large_en_v1_5,
 
         # GIST Models
         #"GIST-Small-Embedding-v0": feature_extractor_gist_small_embedding_v0,
@@ -169,8 +169,8 @@ def run_txt_emb():
 
         # GTE Models
         #"GTE-Small": feature_extractor_gte_small,
-        "GTE-Base": feature_extractor_gte_base,
-        "GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
+        #"GTE-Base": feature_extractor_gte_base,
+        #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
         #"GTE-Large": feature_extractor_gte_large,
 
         # GTR T5 Base
