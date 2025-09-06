191ce17eac4336c89121f3648b188d8797f5e3b0
Oksana Kolomenko <Oksana.Kolomenko@Student.HTW-Berlin.de>
2025-09-06 16:29:18 +0200

    mo hgbc gte_en_base

--- run_setup.py
+++ run_setup.py
@@ -64,8 +64,8 @@ def run_txt_emb():
     ]"""
 
     # === MIMIC ===
-    task = "task_0"
-    dataset = DatasetName.MIMIC_0.value
+    task = "task_1"
+    dataset = DatasetName.MIMIC_1.value
 
     X_train = load_features(f"mimic_data/X_train_{task}.csv")
     X_test = load_features(f"mimic_data/X_test_{task}.csv")
@@ -152,7 +152,7 @@ def run_txt_emb():
 
         # E5 Models
         #"E5-Small-V2": feature_extractor_e5_small_v2,
-        "E5-Base-V2": feature_extractor_e5_base_v2,
+        #"E5-Base-V2": feature_extractor_e5_base_v2,
         #"E5-Large-V2": feature_extractor_e5_large_v2,
 
         # BGE Models (done)
@@ -168,7 +168,7 @@ def run_txt_emb():
         # GTE Models
         #"GTE-Small": feature_extractor_gte_small,
         #"GTE-Base": feature_extractor_gte_base,
-        #"GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
+        "GTE-Base-EN-v1.5": feature_extractor_gte_base_en_v1_5,
         #"GTE-Large": feature_extractor_gte_large,
 
         # GTR T5 Base
